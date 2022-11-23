32421f70f9ed079a2a1cff5c723200d7da72ba40
MazdaRacerBoy <steve.gaetjens@roadrunner.com>
2022-11-23 17:43:11 -0500

    Removed footer text referencing the original project

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -14,13 +14,6 @@ const Footer = () => {
   return !visibleOnlyPath.includes(pathname) ? null : (
     <footer className="footer">
       <div className="footer-col-1">
-        <strong>
-          <span>
-            Developed by
-            {' '}
-            <a href="https://github.com/jgudo">JULIUS GUEVARRA</a>
-          </span>
-        </strong>
       </div>
       <div className="footer-col-2">
         <img alt="Footer logo" className="footer-logo" src={logo} />
@@ -29,13 +22,7 @@ const Footer = () => {
           {new Date().getFullYear()}
         </h5>
       </div>
-      <div className="footer-col-3">
-        <strong>
-          <span>
-            Fork this project &nbsp;
-            <a href="https://github.com/jgudo/ecommerce-react">HERE</a>
-          </span>
-        </strong>
+      <div className="footer-col-3">       
       </div>
     </footer>
   );
