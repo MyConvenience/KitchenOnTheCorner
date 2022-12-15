4b47b24ddbc5d3d54235ea40c719cef9e578f38f
MazdaRacerBoy <steve.gaetjens@roadrunner.com>
2022-12-15 17:04:29 -0500

    Start on content editing - switching computers

--- src/views/admin/content/index.jsx
+++ src/views/admin/content/index.jsx
@@ -1,17 +1,25 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { Boundary } from '@/components/common';
-import { useDocumentTitle, useScrollTop } from '@/hooks';
-import React from 'react';
+import { useContent, useDocumentTitle, useScrollTop } from '@/hooks';
+import React, {useState} from 'react';
 import { useSelector } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import {ListGroup, Button, ButtonGroup} from 'react-bootstrap';
 
 const Content = () => {
+  const {selected, setSelected} = useState(null);
+  const {rotatorPages} = useContent(false);
   useDocumentTitle('User Administration | KOTC Admin');
   useScrollTop();
+
+
 
   return (
     <Boundary>
       <div>Administer site content</div>
+      <ListGroup as='ul'>
+        {rotatorPages.map(p => <ListGroup.Item key={p.name} action onClick={alertClicked}></ListGroup.Item>)}
+      </ListGroup>
     </Boundary>
   );
 };
